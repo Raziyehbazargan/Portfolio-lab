04f7d5a874a61b6c410ce6cea9e5245b5b5f2cf2
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-05 11:23:41 -0800

    correct the how access to html element

--- scripts/articles.js
+++ scripts/articles.js
@@ -12,10 +12,10 @@ function Experience (opts) {
 Experience.prototype.toHtml = function() {
   var $newExperience = $('article.template').clone();
 
-  $newExperience.find('.company').html('<h5><b>'+this.company+'</b></h5>');
-  $newExperience.find('.positionName').html('<h5><span>'+this.positionName+'</span></h5>');
-  $newExperience.find('.city').html('<h5>' + this.city + ' ' + this.date +'</h5>');
-  $newExperience.find('.description').html(this.description);
+  $newExperience.find('h5:first-child').text(this.company);
+  $newExperience.find('h5:nth-child(2)').text(this.positionName);
+  $newExperience.find('p:nth-child(3)').text(this.city +', '+ this.date);
+  $newExperience.find('p:nth-child(4)').text(this.description);
 
   $('#articles').append($newExperience);
   $newExperience.removeClass('template');
--- scripts/data.js
+++ scripts/data.js
@@ -4,13 +4,29 @@ var rawData = [
     positionName:  'Software Specialist',
     city:          'Tehran',
     date:          '2008-2015',
-    description:   '<p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. .</p>'
+    description:   'Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. .'
   },
   {
     company:       'Rahyab Rayaneh Gostar',
     positionName:  'Software Specialist',
     city:          'Tehran',
     date:          '2008-2015',
-    description:   '<p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. .</p>'
+    description:   'Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. .'
   },
+];
+
+var rawEducationData = [
+  {
+    school:        'Code fellows',
+    city:          'Tehran',
+    certificate:   'Certificate Program',
+    date:          '2015-2016'
+
+  },
+  {
+    school:        'Azad University',
+    city:          'BandarAbbas',
+    certificate:   'Bachelor Degree',
+    date:          '2006-2008'
+  }
 ];
