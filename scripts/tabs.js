dc823bbfadb898f4fc27eceafb2f61a06ec43643
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-06 21:22:54 -0800

    add tabs and some css style to about us page still not complete

---
+++ scripts/tabs.js
@@ -0,0 +1,30 @@
+$(function(){
+  $('ul#tab-text li').not('.aboutMe').hide();
+
+  $('ul#tabs li').click(function(e){
+    if (!$(this).hasClass('active')) {
+      var tabNum = $(this).text();
+      $('ul#tabs li.active').removeClass('active');
+      $(this).addClass('active');
+
+      switch (tabNum) {
+      case 'About Me':{
+        $('ul#tab-text li.aboutMe').show();
+        $('ul#tab-text li').not('.aboutMe').hide();
+        break;
+      }
+      case 'Portfolio':{
+        $('ul#tab-text li.portfolio').show();
+        $('ul#tab-text li').not('.portfolio').hide();
+        break;
+      }
+      case 'Resume':{
+        $('ul#tab-text li.resume').show();
+        $('ul#tab-text li').not('.resume').hide();
+        break;
+      }
+      default:$('ul#tabs li.aboutMe').show();
+      }
+    }
+  });
+});
