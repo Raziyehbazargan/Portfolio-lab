09ec2e38b7756445435d825f49c6d2368dbcac5e
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-18 14:58:18 -0800

    correct the $.getJson the problem was in contact.json file in last ','

--- scripts/articles.js
+++ scripts/articles.js
@@ -11,10 +11,10 @@ function Experience (opts) {
 Experience.prototype.toHtml = function() {
   var $newExperience = $('li article.template').clone();
   var htmlScript = $('#projectDate').html();
-  console.log(htmlScript);
+  // console.log(htmlScript);
 
   var handlebarsScript = Handlebars.compile(htmlScript);
-  console.log(handlebarsScript);
+  // console.log(handlebarsScript);
 
   var content = {
     'project': this.project,
@@ -33,7 +33,7 @@ Experience.tabPage = function() {
     $('.main-nav li').removeClass('active');
     $(this).addClass('active');
     $('.box').hide();
-    $('#' + $(this).data('content')).show();
+    $('#' + $(this).data('content')).slideDown('slow');
   });
   $('.main-nav .tab:first').click();
 };
@@ -46,6 +46,29 @@ experiences.forEach(function(a){
   $('li#portfolio').append(a.toHtml());
 });
 
-$(document).ready(function(){
+
+// function loadJsonContactData(){
+//   var htmlEmailLink = '<a href=';
+//   var htmlGithubLink = '<a href=';
+//   var htmllinkeDinLink = '<a href=';
+//   var htmlFacebookLink = '<a href=';
+//   $.getJSON('scripts/contact.json',function(response) {
+//     var $list = $('#contact');
+//     $.each(response,function(index , contactInf) {
+//       htmlEmailLink += '"' + contactInf.email + '"' + '>email</a><br>';
+//       htmlGithubLink += '"' + contactInf.github + '"' + '>github</a><br>';
+//       htmllinkeDinLink += '"' + contactInf.linkedin + '"' + '>linkedin</a><br>';
+//       htmlFacebookLink += '"' + contactInf.facebook + '"' + '>facebook</a><br>';
+//     });
+//
+//     $list.html(htmlEmailLink);
+//     // $list.html(htmlGithubLink);
+//     // $list.html(htmllinkeDinLink);
+//     // $list.html(htmlFacebookLink);
+//   });
+// };
+
+$(function(){
   Experience.tabPage();
+  // loadJsonContactData();
 });
