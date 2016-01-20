6379373744e93402157d6155d44d43e2783f62bf
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-20 15:02:27 -0800

    complete singlePage assignment in my portfolio

--- scripts/routes.js
+++ scripts/routes.js
@@ -2,5 +2,6 @@ page.base('');
 page('/',indexController.index);
 page('/aboutMe',indexController.index);
 page('/portfolio',portfolioController.index);
-
+page('/resume',resumeController.index);
+page('/contact',contactController.index);
 page();
