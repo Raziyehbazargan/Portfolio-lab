c0c89c099e3b445ec4b9b021858ac582ce0943ed
raziyeh bazargan <r.bazargan@gmail.com>
2016-02-04 22:55:29 -0800

    add IIFE

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,77 +1,75 @@
 
+(function(module){
 
-function Experience (opts) {
-  this.project = opts.project;
-  this.institute = opts.institute;
-  this.date = opts.date;
-  this.description = opts.description;
-}
-
-Experience.all=[];
-
-Experience.prototype.toHtml = function() {
-  var htmlScript = $('#projectDate').html();
-  var handlebarsScript = Handlebars.compile(htmlScript);
-  var content = {
-    'project': this.project,
-    'institute': this.institute,
-    'date': this.date,
-    'description': this.description,
-  };
-  var compileHtml = handlebarsScript(content);
-  return compileHtml;
-};
-
-Experience.loadAll = function(rawData) {
-  rawData.forEach(function(ele) {
-    Experience.all.push(new Experience(ele));
-  });
-};
-
-Experience.getAll = function(){
-  $.getJSON('data/data.json',function(rawData){
-    Experience.loadAll(rawData);
-    localStorage.rawData = JSON.stringify(rawData);
-    articleView.initIndexPage();
-  });
-};
-
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
-Experience.fetchAll = function() {
-  if (localStorage.rawData) {
-    $.ajax({   //loking for JSON object
-      type: 'HEAD',  //because we just head of data and dont use get because get get all content of data
-      url:'/data/data.json',
-      success:function(data,message,xhr){
-        console.log(xhr);
-        var eTag = xhr.getResponseHeader('eTag');
-        console.log(eTag);
-        if(!localStorage.eTag || localStorage.eTag !== eTag ){// !localStorage.eTag  -->if eTag doesn;t exist
-          localStorage.eTag = eTag;
-          Experience.getAll();
-        }else {
-          console.log('here 3');
-          Experience.loadAll(JSON.parse(localStorage.rawData));
-          articleView.initIndexPage();
-        }
-      }
-    });
-  }else {
-    Experience.getAll();
+  function Experience (opts) {
+    this.project = opts.project;
+    this.institute = opts.institute;
+    this.date = opts.date;
+    this.description = opts.description;
   }
-};
-Experience.tabPage = function() {
-  $('.main-nav').on('click' , '.tab' , function(){
-    $('.main-nav li').removeClass('active');
-    $(this).addClass('active');
-    $('.box').hide();
-    $('#' + $(this).data('content')).slideDown('slow');
-  });
-  $('.main-nav .tab:first').click();
-};
 
-$(function(){
-  Experience.tabPage();
-  Experience.fetchAll();
-});
+  Experience.all=[];
+
+  Experience.prototype.toHtml = function() {
+    var htmlScript = $('#projectDate').html();
+    var handlebarsScript = Handlebars.compile(htmlScript);
+    var content = {
+      'project': this.project,
+      'institute': this.institute,
+      'date': this.date,
+      'description': this.description,
+    };
+    var compileHtml = handlebarsScript(content);
+    return compileHtml;
+  };
+
+  Experience.loadAll = function(rawData) {
+    rawData.forEach(function(ele) {
+      Experience.all.push(new Experience(ele));
+    });
+  };
+
+  Experience.getAll = function(){
+    $.getJSON('data/data.json',function(rawData){
+      Experience.loadAll(rawData);
+      localStorage.rawData = JSON.stringify(rawData);
+      articleView.initIndexPage();
+    });
+  };
+
+  // This function will retrieve the data from either a local or remote source,
+  // and process it, then hand off control to the View.
+  Experience.fetchAll = function() {
+    if (localStorage.rawData) {
+      $.ajax({   //loking for JSON object
+        type: 'HEAD',  //because we just head of data and dont use get because get get all content of data
+        url:'/data/data.json',
+        success:function(data,message,xhr){
+          console.log(xhr);
+          var eTag = xhr.getResponseHeader('eTag');
+          console.log(eTag);
+          if(!localStorage.eTag || localStorage.eTag !== eTag ){// !localStorage.eTag  -->if eTag doesn;t exist
+            localStorage.eTag = eTag;
+            Experience.getAll();
+          }else {
+            console.log('here 3');
+            Experience.loadAll(JSON.parse(localStorage.rawData));
+            articleView.initIndexPage();
+          }
+        }
+      });
+    }else {
+      Experience.getAll();
+    }
+  };
+  Experience.tabPage = function() {
+    $('.main-nav').on('click' , '.tab' , function(){
+      $('.main-nav li').removeClass('active');
+      $(this).addClass('active');
+      $('.box').hide();
+      $('#' + $(this).data('content')).slideDown('slow');
+    });
+    $('.main-nav .tab:first').click();
+  };
+  module.Experience = Experience;
+})(window);
---
+++ scripts/githubToken.js
@@ -0,0 +1,1 @@
+ var gitHubToken = '13864a1f6691d6cfa47300cd36d0441ba9eccd06';
